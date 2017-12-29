9a9b54269187490e8be66889ac6f03cf3a119e70
scott_he <heshtjs@163.com>
2017-12-29 15:22:21 +0800

    java 文件地址问题

--- aarexemple/src/main/java/com/example/scott/AarTestUtil.java
+++ aarexemple/src/main/java/com/example/scott/AarTestUtil.java
@@ -1,11 +1,13 @@
+package com.example.scott;
+
 import android.util.Log;
 
 /**
  * Created by admin on 2017/12/29.
  */
 
-public class TestUtil {
-    private static final String TAG = "TestUtil";
+public class AarTestUtil {
+    private static final String TAG = "AarTestUtil";
     public static void logAar(){
         Log.e(TAG, "logAar: 测试Aar 上传到jcenter");
     }
