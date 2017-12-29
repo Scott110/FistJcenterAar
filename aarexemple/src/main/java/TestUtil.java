ae9cad9920d87a91947a1684d6ff9e62103d4775
scott_he <heshtjs@163.com>
2017-12-29 10:28:34 +0800

    第一次提交

---
+++ aarexemple/src/main/java/TestUtil.java
@@ -0,0 +1,12 @@
+import android.util.Log;
+
+/**
+ * Created by admin on 2017/12/29.
+ */
+
+public class TestUtil {
+    private static final String TAG = "TestUtil";
+    public static void logAar(){
+        Log.e(TAG, "logAar: 测试Aar 上传到jcenter");
+    }
+}
